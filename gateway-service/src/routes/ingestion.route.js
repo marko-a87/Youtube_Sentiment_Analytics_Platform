8469c6843c5dfac901a322490de238e5e3862ff2
Shamar Malcolm <malco0253@gmail.com>
2026-07-31 22:27:23 -0500

    feat: add v1 for api ingestion route

---
+++ gateway-service/src/routes/ingestion.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/api/v1/ingestion", (req, res) => {
+  res.status(200).json({ message: "Ingestion route is working!" });
+});
+
+export default router;
