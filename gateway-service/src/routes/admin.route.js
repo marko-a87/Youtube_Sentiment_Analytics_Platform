ce79c24757585c6e962490ae7d97d2da367902d8
Shamar Malcolm <malco0253@gmail.com>
2026-07-31 22:22:01 -0500

    feat: add v1 for api admin route

--- gateway-service/src/routes/admin.route.js
+++ gateway-service/src/routes/admin.route.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/api/admin", (req, res) => {
+router.get("/api/v1/admin", (req, res) => {
   res.status(200).json({ message: "Admin route is working!" });
 });
 
